b02f9fef9a5402f3b7179cbafe9b277199d2c6bf
DaviFaustino <davifaustino05@gmail.com>
2024-04-11 20:47:52 -0300

    feat: implement option selection in gerenciamento

---
+++ src/components/BotaoOptionGerenView.vue
@@ -0,0 +1,25 @@
+<template>
+    <button @click="emitirEvento" class="h-7 w-48 mr-1 mb-1 bg-orange-200 hover:bg-orange-300 duration-100 rounded-md">
+        {{ buttonText }}
+    </button>
+</template>
+
+<script>
+export default {
+    props: {
+        buttonText: {
+            type: String,
+            required: true
+        },
+        optionId: {
+            type: String,
+            required: true
+        }
+    },
+    methods: {
+        emitirEvento() {
+            this.$emit('clique', this.optionId);
+        }
+    }
+}
+</script>
--- src/views/GerenciamentoView.vue
+++ src/views/GerenciamentoView.vue
@@ -1,27 +1,47 @@
 <script setup>
-import BotaoGeren1View from '/src/components/BotaoGeren1View.vue'
+import BotaoOptionGerenView from '/src/components/BotaoOptionGerenView.vue'
 </script>
 
 <template>
-    <h1 class="my-5">Escolha uma opção abaixo</h1>
+   <h1 class="my-5">Escolha uma opção abaixo</h1>
 
-    <div>
-        <div class="flex flex-col md:flex-row w-fit h-fit">
-            <BotaoGeren1View buttonText="Registrar Tipo"/>
-            <BotaoGeren1View buttonText="Registrar Produto"/>
-        </div>
-        <div class="flex flex-col md:flex-row w-fit h-fit">
-            <BotaoGeren1View buttonText="Atualizar Tipo"/>
-            <BotaoGeren1View buttonText="Atualizar Produto"/>
-        </div>
-        <div class="flex flex-col md:flex-row w-fit h-fit">
-            <BotaoGeren1View buttonText="Deletar Tipo"/>
-            <BotaoGeren1View buttonText="Deletar Produto"/>
-        </div>
-    </div>
+   <div>
+      <div class="flex flex-col md:flex-row w-fit h-fit">
+         <BotaoOptionGerenView @clique="exibirOption" buttonText="Registrar Tipo" optionId="r-tipo"/>
+         <BotaoOptionGerenView @clique="exibirOption" buttonText="Registrar Produto" optionId="r-produto"/>
+      </div>
+      <div class="flex flex-col md:flex-row w-fit h-fit">
+         <BotaoOptionGerenView @clique="exibirOption" buttonText="Atualizar Tipo" optionId="a-tipo"/>
+         <BotaoOptionGerenView @clique="exibirOption" buttonText="Atualizar Produto" optionId="a-produto"/>
+      </div>
+      <div class="flex flex-col md:flex-row w-fit h-fit">
+         <BotaoOptionGerenView @clique="exibirOption" buttonText="Deletar Tipo" optionId="d-tipo"/>
+         <BotaoOptionGerenView @clique="exibirOption" buttonText="Deletar Produto" optionId="d-produto"/>
+      </div>
+   </div>
 
-    <div class="h-1 w-4/5 bg-orange-600 my-10"></div>
+   <div class="h-1 w-4/5 bg-orange-600 my-10">
+      <form action=""></form>
+   </div>
 
-    
+   <div id="r-tipo" class="option hidden"></div>
+   <div id="r-produto" class="option hidden"></div>
+   <div id="a-tipo" class="option hidden"></div>
+   <div id="a-produto" class="option hidden"></div>
+   <div id="d-tipo" class="option hidden"></div>
+   <div id="d-produto" class="option hidden"></div>
 </template>
 
+<script>
+export default {
+   methods: {
+      exibirOption(optionId) {
+         const lili = document.getElementsByClassName("option");
+         for (let index = 0; index < lili.length; index++) {
+               lili.item(index).classList.add("hidden");
+         }
+         document.getElementById(optionId).classList.remove("hidden");
+      }
+   }
+}
+</script>
