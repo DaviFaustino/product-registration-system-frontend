a03901d92586c53b3738eba435cfac4f474cb6b2
DaviFaustino <davifaustino05@gmail.com>
2024-09-06 15:40:21 -0300

    fix: corrigir responsiv. de atualizar tipo

--- src/components/AtualizarTipoComp.vue
+++ src/components/AtualizarTipoComp.vue
@@ -189,7 +189,7 @@ onMounted(() => {
          </div>
       </form>
 
-      <div class="h-0.5 w-96 bg-orange-400 mt-8 mb-2"></div>
+      <div class="h-0.5 w-full sm:w-96 bg-orange-400 mt-8 mb-2"></div>
 
       <form @submit.prevent="enviarFormulario" id="update-area-tipo" class="mt-8 space-y-2 opacity-20" autocomplete="off">
          <div>
@@ -224,7 +224,7 @@ onMounted(() => {
          <div class="h-full"></div>
 
          <div class="flex">
-            <button type="button" id="bt-restaurar" disabled="true" @click="restaurar" class="h-7 w-7 mr-32 bg-orange-500 border-2 border-orange-600 hover:bg-orange-600 duration-150 rounded-lg mt-5 text-center text-white font-bold">
+            <button type="button" id="bt-restaurar" disabled="true" @click="restaurar" class="h-7 w-7 mr-24 bg-orange-500 border-2 border-orange-600 hover:bg-orange-600 duration-150 rounded-lg mt-5 text-center text-white font-bold">
                <svg class="h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor">
                   <path d="M463.5 224H472c13.3 0 24-10.7 24-24V72c0-9.7-5.8-18.5-14.8-22.2s-19.3-1.7-26.2 5.2L413.4 96.6c-87.6-86.5-228.7-86.2-315.8 1c-87.5 87.5-87.5 229.3 0 316.8s229.3 87.5 316.8 0c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0c-62.5 62.5-163.8 62.5-226.3 0s-62.5-163.8 0-226.3c62.2-62.2 162.7-62.5 225.3-1L327 183c-6.9 6.9-8.9 17.2-5.2 26.2s12.5 14.8 22.2 14.8H463.5z"/>
                </svg>
