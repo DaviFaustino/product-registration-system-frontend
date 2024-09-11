cb434c7574fa7692186cd606d3f9481acadec6fd
DaviFaustino <davifaustino05@gmail.com>
2024-09-11 18:54:57 -0300

    feat: realizar busca do produto depois da leitura do codigo

--- src/components/BuscarProdutoComp.vue
+++ src/components/BuscarProdutoComp.vue
@@ -17,6 +17,7 @@ const backendURL = import.meta.env.VITE_BACKEND_URL;
 
 function atualizarBusca(novoCodigo) {
    busca.value = novoCodigo;
+   realizarBuscaProduto();
 }
 
 function buscarTipos() {
